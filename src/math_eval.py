e50f53b1e3565a431e5e5ceb6d37817b69ae80ae
Armend Veseli <60117923+armendveseli@users.noreply.github.com>
2022-06-02 20:45:31 -0400

    Create math_eval.py

---
+++ src/math_eval.py
@@ -0,0 +1,18 @@
+def eval_math(expression: str):
+    #Check for numbers.
+    if not any(char.isdigit() for char in expression):
+        return 0
+    
+    #Check for letters (upper and lower case)
+    elif any(char.isalpha() for char in expression.lower()):
+        return 0
+		
+    #Check for operators.
+    if not any(sub_str in ["+", "-", "*", "/", "%", "//", "**"] for sub_str in expression):
+        return 0
+
+    #Everything seems secure.
+    try:
+        return eval(expression)
+    except:
+        raise ArithmeticError("Invalid expression.")

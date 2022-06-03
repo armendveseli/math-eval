8b17a6dbe27fb194fb7c008053b1c573c3f27d8b
Armend Veseli <60117923+armendveseli@users.noreply.github.com>
2022-06-03 11:20:49 -0400

    Update math_eval.py

--- src/math_eval.py
+++ src/math_eval.py
@@ -1,13 +1,17 @@
 def eval_math(expression: str):
-    #Check for no numbers.
+    #Block due to numeric absence.
     if not any(char.isdigit() for char in expression):
         return 0
     
-    #Check for letters (upper and lower case)
+    #Block due to presence of special characters.
+    elif any(sub_chr in [":", "="] for sub_chr in expression):
+        return 0
+	
+    #Block letters (upper and lower case).
     elif any(char.isalpha() for char in expression.lower()):
         return 0
 		
-    #Check for operators.
+    #Block due to operator absence.
     elif not any(sub_str in ["+", "-", "*", "/", "%", "//", "**"] for sub_str in expression):
         return 0
 
